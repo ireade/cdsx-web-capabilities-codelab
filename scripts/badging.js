703d47513827b72d6c8c214230ab4a31951c0075
Ire Aderinokun <ireaderinokun@Ires-iMac.local>
2021-02-09 22:09:51 +0000

    file system api

---
+++ scripts/badging.js
@@ -0,0 +1,9 @@
+let counter = 0;
+
+document.getElementById('badge').addEventListener('click', () => {
+    navigator.setAppBadge(++counter);
+});
+
+document.getElementById('clear-badge').addEventListener('click', () => {
+    navigator.clearAppBadge()
+});
---
+++ scripts/file-system.js
@@ -0,0 +1,20 @@
+let fileHandle;
+
+const textarea = document.getElementById('textarea');
+
+document.getElementById('open-file-picker').addEventListener('click', async () => {
+    [fileHandle] = await window.showOpenFilePicker();
+    const file = await fileHandle.getFile();
+    const contents = await file.text();
+    textarea.value = contents;
+  });
+
+document.getElementById('save-file').addEventListener('click', async () => {
+    
+    const writable = await fileHandle.createWritable();
+
+    await writable.write(textarea.value);
+
+    await writable.close();
+
+  });
