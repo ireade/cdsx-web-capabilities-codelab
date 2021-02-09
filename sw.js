8957d1650ca78b4951d2b72dbb126f972ee606b4
Ire Aderinokun <ireaderinokun@Ires-iMac.local>
2021-02-09 21:45:42 +0000

    Setup

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+    self.skipWaiting()
+  });
+  
+  self.addEventListener('activate', event => {
+    
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+    );
+  });
